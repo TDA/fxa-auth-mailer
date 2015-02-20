e02f7fde6bac34fde55de6e854d409e9e90a2ccd
Shane Tomlinson <stomlinson@mozilla.com>
2015-02-20 16:46:13 +0000

    feat: Add change password, reset password, and new sync device notification emails.
    fixes #24

--- mailer.js
+++ mailer.js
@@ -21,10 +21,11 @@ module.exports = function (log) {
         pass: config.password
       }
     }
-    
+
     this.mailer = nodemailer.createTransport('SMTP', options)
     this.sender = config.sender
     this.verificationUrl = config.verificationUrl
+    this.initiatePasswordResetUrl = config.initiatePasswordResetUrl
     this.passwordResetUrl = config.passwordResetUrl
     this.accountUnlockUrl = config.accountUnlockUrl
     this.translator = translator
@@ -160,6 +161,82 @@ module.exports = function (log) {
         'X-Service-ID': message.service,
         'X-Link': link,
         'Content-Language': translator.language
+      }
+    }
+    return this.send(email)
+  }
+
+  Mailer.prototype.passwordChangedEmail = function (message) {
+    log.trace({ op: 'mailer.passwordChangedEmail', email: message.email, uid: message.uid })
+    var query = {
+      email: message.email
+    }
+    var link = this.initiatePasswordResetUrl + '?' + qs.stringify(query)
+
+    var translator = this.translator(message.acceptLanguage)
+    var values = {
+      translator: translator,
+      resetLink: link
+    }
+    var localized = this.templates.passwordChangedEmail(values)
+    var email = {
+      sender: this.sender,
+      to: message.email,
+      subject: translator.gettext('Your Firefox Account password has been changed'),
+      text: localized.text,
+      html: localized.html,
+      headers: {
+        'Content-Language': translator.language,
+        'X-Link': link
+      }
+    }
+    return this.send(email)
+  }
+
+  Mailer.prototype.passwordResetEmail = function (message) {
+    log.trace({ op: 'mailer.passwordiResetEmail', email: message.email, uid: message.uid })
+    var query = {
+      email: message.email
+    }
+    var link = this.initiatePasswordResetUrl + '?' + qs.stringify(query)
+
+    var translator = this.translator(message.acceptLanguage)
+    var values = {
+      translator: translator,
+      resetLink: link
+    }
+    var localized = this.templates.passwordResetEmail(values)
+    var email = {
+      sender: this.sender,
+      to: message.email,
+      subject: translator.gettext('Your Firefox Account password has been reset'),
+      text: localized.text,
+      html: localized.html,
+      headers: {
+        'Content-Language': translator.language,
+        'X-Link': link
+      }
+    }
+    return this.send(email)
+  }
+
+  Mailer.prototype.newSyncDeviceEmail = function (message) {
+    log.trace({ op: 'mailer.newSyncDeviceEmail', email: message.email, uid: message.uid })
+
+    var translator = this.translator(message.acceptLanguage)
+    var values = {
+      translator: translator
+    }
+    var localized = this.templates.newSyncDeviceEmail(values)
+    var email = {
+      sender: this.sender,
+      to: message.email,
+      subject: translator.gettext('A new device is now syncing to your Firefox Account'),
+      text: localized.text,
+      html: localized.html,
+      headers: {
+        'Content-Language': translator.language
+        // No X-Link header is specified - the email does not contain a link.
       }
     }
     return this.send(email)
